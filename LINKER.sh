f48e1bbaf4cec85e707da1fb28ca1d85190aa55a
Santiago Gonzalez <slgonzalez@me.com>
2015-04-28 13:48:14 -0600

    Initial git commit.

---
+++ LINKER.sh
@@ -0,0 +1,9 @@
+# you should run this to get all the correct symlinks to config files and stuff
+
+ln -s ~/Dropbox/scripts/utils/power.sh /usr/local/bin/power.sh
+
+ln -s ~/Dropbox/scripts/dotzsh/zshrc ~/.zshrc
+ln -s ~/Dropbox/scripts/dotzsh/ ~/.zsh
+ln -s ~/Dropbox/scripts/dotvim/vimrc ~/.vimrc
+ln -s ~/Dropbox/scripts/dotvim/ ~/.vim
+ln -s ~/Dropbox/scripts/tmux/tmux.conf ~/.tmux.conf
